9317e4b5a15977b6824c98169085765786f72e17
lolo35250 <40354933+lolo35250@users.noreply.github.com>
2022-06-14 22:55:17 +0200

    Update install_tools_for_ctf.sh

--- install_tools_for_ctf.sh
+++ install_tools_for_ctf.sh
@@ -87,27 +87,3 @@ ln -s /usr/share/spectrology/ /usr/bin
 #forensic
 echo -e '\033[0;31m *********** Foremost ***********\033[m'
 apt install foremost -y
-
-
-# Osint
-echo -e '\033[0;31m *********** Daprofiler ***********\033[m'
-git clone https://github.com/TheRealDalunacrobate/DaProfiler.git /usr/share/DaProfiler
-cd /usr/share/DaProfiler
-pip install -r requirements.txt
-
-echo -e '\033[0;31m *********** Holele ***********\033[m'
-git clone https://github.com/megadose/holehe.git /usr/share/holele
-cd /usr/share/holele
-python3 setup.py install
-
-echo -e '\033[0;31m *********** Sherlock ***********\033[m'
-git clone https://github.com/sherlock-project/sherlock.git /usr/share/sherlock
-cd /usr/share/sherlock
-python3 -m pip install -r requirements.txt
-
-echo -e '\033[0;31m *********** Moriarty ***********\033[m'
-
-git clone https://github.com/AzizKpln/Moriarty-Project /usr/share/Moriarty
-cd /usr/share/Moriarty
-chmod 755 install.sh
-bash install.sh
