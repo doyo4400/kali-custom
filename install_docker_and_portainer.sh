a979767eebdabac0559d24f963abc2b62bb7efba
doyo4400 <40354933+doyo4400@users.noreply.github.com>
2022-08-30 19:11:17 +0200

    Update install_docker_and_portainer.sh

--- install_docker_and_portainer.sh
+++ install_docker_and_portainer.sh
@@ -14,14 +14,15 @@ apt-get install \
     lsb-release
 	
 	
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-	
+sudo mkdir -p /etc/apt/keyrings
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /etc/apt/keyrings/docker.gpg
+
 echo \
-  "deb [arch=amd64 signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.gpg] https://download.docker.com/linux/debian \
   $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
   
 apt-get update
-apt-get install docker-ce docker-ce-cli containerd.io -y  
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-compose-plugin -y
 apt install docker-compose -y
 adduser $USER docker
 
