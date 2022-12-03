abdbfc5a59e8296303612ed45920e3088ef8a6c2
doyo4400 <40354933+doyo4400@users.noreply.github.com>
2022-12-03 12:22:40 +0100

    Update delete_password_of_pdf.sh

--- delete_password_of_pdf.sh
+++ delete_password_of_pdf.sh
@@ -1,3 +1,3 @@
 sudo apt install qpdf -y 
 
-qpdf --password='the_password' --decrypt name_of_pdf.pdf name_of_pdf_withoutpwd.pdf
+qpdf --password='the_password' --decrypt $name_of_pdf.pdf $name_of_pdf_withoutpwd.pdf
